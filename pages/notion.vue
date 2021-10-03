c98f514ad4153a01622dd85d8838254166be3b3b
Junchi <benson26365156@gmail.com>
2021-10-03 12:17:31 +0800

    feat: add Notion redirect url

---
+++ pages/notion.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notionUrl: 'https://chi0307.notion.site/Website-93fcfd9c08644812a467cd8c688d7108',
+    };
+  },
+  created() {
+    // eslint-disable-next-line
+    window.location.href = this.notionUrl;
+  },
+};
+</script>
