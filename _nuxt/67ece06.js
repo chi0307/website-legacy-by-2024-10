260c4efc34eb26ab523675d6362dcdffc560481c
Junchi <benson26365156@gmail.com>
2024-11-12 00:51:21 +0800

    deploy: rename base url

--- _nuxt/67ece06.js
+++ _nuxt/67ece06.js
@@ -1,1 +1,30 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{293:function(e,t,n){"use strict";n.r(t);n(41),n(54);var c={layout:"website",created:function(){this.$router.replace({path:"/aboutMe"})}},l=n(29),component=Object(l.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"index-page"})}),[],!1,null,null,null);t.default=component.exports}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [7],
+  {
+    293: function (e, t, n) {
+      'use strict';
+      n.r(t);
+      n(41), n(54);
+      var c = {
+          layout: 'website',
+          created: function () {
+            this.$router.replace({ path: '/website-legacy-by-2024-10/aboutMe' });
+          },
+        },
+        l = n(29),
+        component = Object(l.a)(
+          c,
+          function () {
+            var e = this.$createElement;
+            return (this._self._c || e)('div', { staticClass: 'index-page' });
+          },
+          [],
+          !1,
+          null,
+          null,
+          null
+        );
+      t.default = component.exports;
+    },
+  },
+]);
