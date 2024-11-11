86951fbd9a18f23aa61d5f857fa4de7484f21f1b
Junchi <benson26365156@gmail.com>
2024-11-12 00:47:29 +0800

    deploy

---
+++ _nuxt/67ece06.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{293:function(e,t,n){"use strict";n.r(t);n(41),n(54);var c={layout:"website",created:function(){this.$router.replace({path:"/aboutMe"})}},l=n(29),component=Object(l.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"index-page"})}),[],!1,null,null,null);t.default=component.exports}}]);
